fb5c4e130587e8d529a708f382667fb64a113a52
Yuki <choppin.block.party@gmail.com>
2018-11-25 17:53:00 +0100

    Enable LLVM_LTO

--- build-llvm.sh
+++ build-llvm.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
+
 set -e
 set +x
 
@@ -55,11 +56,9 @@ clone_update_git_repo https://github.com/llvm-mirror/compiler-rt $BRANCH
 # cd "$SCRIPT_DIR/llvm/projects"
 # svn co http://llvm.org/svn/llvm-project/test-suite/trunk test-suite
 
-
-
 cd "$SCRIPT_DIR/llvm"
 mkdir -p .build
 cd .build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local ../
+cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_LTO=ON -DCMAKE_INSTALL_PREFIX=/usr/local ../
 make -j 4
-sudo make insall
+sudo make install
